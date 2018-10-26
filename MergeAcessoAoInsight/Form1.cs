add7e5d84d6b3152726bbc91c4173349af94dd5a
Andre Ribeiro Barros <andre.tode@gmail.com>
2018-10-26 11:17:43 -0300

    Initial commit

---
+++ MergeAcessoAoInsight/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MergeAcessoAoInsight
+{
+    public partial class Form1 : Form
+    {
+        private static string path = @"E:\AcessoAoInsight\merge\";
+        private static string arquivoDestino = path + "arquivo-unico.html";
+        private static string arquivoHR = path + "arquivo-unico2.html";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<string> listaDeArquivos;
+
+            try
+            {
+                Cursor.Current = Cursors.WaitCursor;
+                listaDeArquivos = GetArquivos();
+                listaDeArquivos = OrdenarArquivos(listaDeArquivos);
+                MergeArquivos(listaDeArquivos);
+                Util.LimparArquivo(arquivoDestino);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                Cursor.Current = Cursors.Default;
+            }
+
+            MessageBox.Show("Processamento concluído");
+        }
+
+        private List<string> OrdenarArquivos(List<string> listaDeArquivos)
+        {
+            string sigla = textBoxSigla.Text;
+            string padrao = $"{sigla}\\.[0-9]+.php";
+            Regex r = new Regex(padrao, RegexOptions.Singleline);
+            var listaIntermediaria = new List<ArqOrnado>();
+
+            foreach(var arq in listaDeArquivos)
+            {
+                string trecho = r.Match(arq).ToString();
+
+                if (trecho == "")
+                    throw new Exception("Não foi encontrado o numero do sutta");
+
+                string numStr = trecho.Replace(sigla+".", "").Replace(".php", "");
+
+                listaIntermediaria.Add(new ArqOrnado { num = int.Parse(numStr), nome = arq });
+            }
+
+            return listaIntermediaria.OrderBy(l => l.num).Select(l => l.nome).ToList();
+        }
+
+        class ArqOrnado {
+            public int num { get; set; }
+            public string nome { get; set; }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Cursor.Current = Cursors.WaitCursor;
+            string conteudoArqFinal = File.ReadAllText(arquivoDestino, Util.encoding);
+
+            conteudoArqFinal = AjustarTagHr(conteudoArqFinal);
+            Util.TrocarLinkImportantePorTexto(ref conteudoArqFinal, path);
+            conteudoArqFinal = Util.EncontraLinksRemover(conteudoArqFinal);
+            File.WriteAllText(arquivoHR, conteudoArqFinal, Util.encoding);
+            Cursor.Current = Cursors.Default;
+            MessageBox.Show("Processamento concluído");
+        }
+
+        private string AjustarTagHr(string conteudo)
+        {
+            string hrIncluir = "<hr size=2 width=\"100%\"><p class=Normal>&nbsp;</p>";
+            string tagTitulo = "<p class=Tit3 align=center style='text-align:center'>";
+            string tagParagrafo1 = "<p class=Normal><span lang=PT-BR style='mso-ansi-language:PT-BR'>";
+            string hrProcurar = "<hr size=2 width=\"100%\" align=center>";
+
+            int posTitulo1 = 0;
+            int posParagrafo1 = 0;
+            int posHr = 0;
+            while (posParagrafo1 < conteudo.Length && posTitulo1 != -1 && posParagrafo1 != -1)
+            {
+                posTitulo1 = conteudo.IndexOf(tagTitulo, posParagrafo1);
+                posParagrafo1 = conteudo.IndexOf(tagParagrafo1, posTitulo1 + tagTitulo.Length);
+
+                if (posTitulo1 != -1)
+                    posHr = conteudo.IndexOf(hrProcurar, posTitulo1);
+
+                if (posHr != -1 && posParagrafo1 != -1 && posHr < posParagrafo1)
+                    conteudo = conteudo.Remove(posHr, hrProcurar.Length);
+
+                if (posTitulo1 != -1)
+                    conteudo = conteudo.Insert(posTitulo1 - 1, hrIncluir);
+            }
+
+            return conteudo;
+        }
+
+        private void MergeArquivos(List<string> listaDeArquivos)
+        {
+            File.WriteAllText(arquivoDestino, "", Util.encoding);
+            string conteudoInicio = File.ReadAllText(path + "topo.inc", Util.encoding);
+            File.AppendAllText(arquivoDestino, conteudoInicio, Util.encoding);
+
+            foreach (var arq in listaDeArquivos)
+            {
+                var conteudoMeio = Util.ExtrairSuttaArquivo(arq);
+                Util.RemoverAvisoDistribuicao(ref conteudoMeio);
+                File.AppendAllText(arquivoDestino, conteudoMeio, Util.encoding);
+            }
+
+            string conteudoFim = File.ReadAllText(path + "base.inc", Util.encoding);
+            File.AppendAllText(arquivoDestino, conteudoFim, Util.encoding);
+        }
+
+        private List<string> GetArquivos(string padrao = "*.php")
+        {
+            if (Directory.Exists(path))
+            {
+                // This path is a directory
+                return ProcessDirectory(path, padrao);
+            }
+            else
+            {
+                throw new Exception($"Diretório [{path}] não existe!");
+            }
+        }
+
+        // Process all files in the directory passed in, recurse on any directories 
+        // that are found, and process the files they contain.
+        public List<string> ProcessDirectory(string targetDirectory, string padrao)
+        {
+            var listaDeArquivos = new List<string>();
+
+            // Process the list of files found in the directory.
+            string[] fileEntries = Directory.GetFiles(targetDirectory, padrao);
+            foreach (string fileName in fileEntries)
+                listaDeArquivos.Add(fileName);
+
+            // Recurse into subdirectories of this directory.
+            //string[] subdirectoryEntries = Directory.GetDirectories(targetDirectory);
+            //foreach (string subdirectory in subdirectoryEntries)
+            //    ProcessDirectory(subdirectory);
+
+            return listaDeArquivos;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            VerificarLinkImportante();
+        }
+
+        private void VerificarLinkImportante()
+        {
+            path = @"E:\AcessoAoInsight\sutta\";
+            List<string> listaDeArquivos = GetArquivos($"{textBoxSigla.Text}*.php");
+
+            foreach (var arq in listaDeArquivos)
+            {
+                var html = File.ReadAllText(arq, Util.encoding);
+                var padrao = @"<a href=\""..\/arquivo_textos_theravada\/[a-zA-Z0-9]+\.php\"">.*<\/a>";
+                Regex r = new Regex(padrao);
+                Match m = r.Match(html);
+
+                while (m.Success)
+                {
+                    Console.WriteLine($"Encontrou no arquivo {arq} - posicao: {m.Index} - match: {m.ToString()}");
+                    m = m.NextMatch(); //vai para o proximo
+                }
+            }
+        }
+    }
+}
---
+++ MergeAcessoAoInsight/Util.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MergeAcessoAoInsight
+{
+    public static class Util
+    {
+        public static Encoding encoding = Encoding.GetEncoding("ISO-8859-1");
+        public static string padraoRemoverLink = @"<a( |\r\n|\n|\r)+(href|name)=(.|\r\n|\n)*?<\/a>";
+
+        public static string EncontraLinksRemover(string html)
+        {
+            //string padrao1 = @"<a[ |\r\n]+href=\""[#a-zA-Z0-9 \.\/_]+\"">([a-zA-Z0-9À-ÿ ,\.\/=<>\-':]|\r\n)+<\/a>";
+            //string padrao2 = @"<a href=\""[#a-zA-Z0-9 \.\/_]+\"">([a-zA-Z0-9À-ÿ ,\.\/=<>\-':]|\r\n)+<\/a>";
+            //Remover(ref html, padrao1);
+            RemoverLink(ref html, padraoRemoverLink);
+
+            //remove o [Retorna]
+            string padraoRetorna = @"\[[a-zA-Z0-9À-ÿ ,\.\/=<>\-':|\r\n|\n|\r]*Retorna(.|\r\n|\n|\r)*?]";
+            RemoverPadrao(ref html, padraoRetorna);
+
+            return html;
+        }
+
+        private static void RemoverLink(ref string html, string padrao)
+        {
+            int posicaoCorrigida = 0;
+            Regex r = new Regex(padrao);
+            Match m = r.Match(html);
+            
+            while (m.Success)
+            {
+                html = html.Remove(m.Index + posicaoCorrigida, m.Length); //remove todo o link
+                var substituir = RemoverString(m.ToString(), "<a", ">");
+                substituir = substituir.Replace("</a>", "");
+                html = html.Insert(m.Index + posicaoCorrigida, substituir); //substituiu colocando tudo que foi removido menos o link
+
+                posicaoCorrigida += substituir.Length - m.Length;
+
+                //########################################################
+                // está travando aqui quando vai pegar o próximo match
+                // Colocar condition: m.Index>35889
+                //########################################################
+                
+                //var task = Task.Run(() => {
+                //    m = m.NextMatch(); //vai para o proximo
+                //});
+                //if (!task.Wait(TimeSpan.FromSeconds(5)))
+                //    break;
+
+                m = m.NextMatch(); //vai para o proximo
+            }
+        }
+
+        private static void RemoverPadrao(ref string html, string padrao)
+        {
+            int posicaoCorrigida = 0;
+            Regex r = new Regex(padrao);
+            Match m = r.Match(html);
+
+            while (m.Success)
+            {
+                //Console.WriteLine("Match: " + m.ToString());
+
+                html = html.Remove(m.Index + posicaoCorrigida, m.Length); //remove todo
+
+                posicaoCorrigida -= m.Length;
+                m = m.NextMatch(); //vai para o proximo
+            }
+        }
+
+        public static string RemoverString(string textoOriginal, string inicio, string fim)
+        {
+            try
+            {
+                int posInicial = 0;
+                int posFim = 0;
+                while (posInicial != -1 && posFim != -1)
+                {
+                    posInicial = textoOriginal.IndexOf(inicio, posFim);
+                    posFim = posInicial != -1 ? textoOriginal.IndexOf(fim, posInicial) : -1;
+                    int tam = (posFim + fim.Length) - posInicial;
+
+                    if (posInicial != -1 && posFim != -1)
+                    {
+                        textoOriginal = textoOriginal.Remove(posInicial, tam);
+                        posFim -= tam;
+                    }
+                }
+
+                return textoOriginal;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private static void GuardarLinkImportante(ref string html)
+        {
+            var padrao = @"<a href=\""..\/arquivo_textos_theravada\/[a-zA-Z0-9]+\.php\"">.*<\/a>";
+            Regex r = new Regex(padrao);
+            Match m = r.Match(html);
+
+            while (m.Success)
+            {
+                //Console.WriteLine("Match: " + m.ToString());
+
+                html = html.Remove(m.Index, m.Length); //remove todo o link
+                var substituir = m.ToString().Replace("<a ", "<x ");
+                html = html.Insert(m.Index, substituir); //substituiu
+
+                m = r.Match(html);
+                m = m.NextMatch(); //vai para o proximo
+            }
+        }
+
+        private static void RecuperarLinkImportante(ref string html)
+        {
+            var padrao = @"<x href=\""..\/arquivo_textos_theravada\/[a-zA-Z0-9]+\.php\"">.*<\/a>";
+            Regex r = new Regex(padrao);
+            Match m = r.Match(html);
+
+            while (m.Success)
+            {
+                html = html.Remove(m.Index, m.Length); //remove todo o link
+                var substituir = m.ToString().Replace("<x ", "<a ");
+                html = html.Insert(m.Index, substituir); //substituiu
+
+                m = r.Match(html);
+                m = m.NextMatch(); //vai para o proximo
+            }
+        }
+
+        public static void TrocarLinkImportantePorTexto(ref string html, string path)
+        {
+            int posicaoAdicao = 0;
+            var padrao = @"<p .*<a href=\""..\/arquivo_textos_theravada\/[a-zA-Z0-9]+\.php\"">.*<\/a>.*<\/p>";
+            Regex r = new Regex(padrao);
+            Match m = r.Match(html);
+
+            while (m.Success)
+            {
+                var textoDoLink = PegarTextoLink(m.ToString(), path);
+                textoDoLink = Util.RemoverString(textoDoLink, "<hr", ">");
+                RemoverLink(ref textoDoLink, padraoRemoverLink);
+                //RemoverAvisoDistribuicao(ref textoDoLink);
+                //textoDoLink = Util.RemoverString(textoDoLink, "<span lang=PT-BR style='font-size:" + Environment.NewLine +
+                //    "7.0pt;mso-ansi-language:PT-BR'><b> Somente para distribuição gratuita.</b><br>",
+                //    "De outra forma todos os direitos estão reservados. </i> <o:p></o:p></span></p>");
+
+                var pontilhado = "<p>............................................................................................................................</p><br>";
+                textoDoLink = pontilhado + textoDoLink + pontilhado;
+                html = html.Insert(m.Index + m.Length + posicaoAdicao, textoDoLink); //substituiu
+                
+                m = m.NextMatch(); //vai para o proximo
+                posicaoAdicao += textoDoLink.Length;
+            }
+        }
+
+        public static string LimparArquivo(string arquivoDestino, bool salvarConteudo = true)
+        {
+            string conteudoArqFinal = File.ReadAllText(arquivoDestino, Util.encoding);
+
+            conteudoArqFinal = Util.RemoverString(conteudoArqFinal, "<!", ">");
+            conteudoArqFinal = Util.RemoverString(conteudoArqFinal, "<img", ">");
+            conteudoArqFinal = Util.RemoverString(conteudoArqFinal, "<b> >> Próximo Sutta", "</p>");
+
+            if (salvarConteudo)
+                File.WriteAllText(arquivoDestino, conteudoArqFinal, Util.encoding);
+
+            return conteudoArqFinal;
+        }
+
+        public static string ExtrairSuttaConteudo(string conteudo)
+        {
+            var separadores = new string[2];
+            separadores[0] = "<!-- INICIO DO TEXTO -->";
+            separadores[1] = "<!-- FIM DO TEXTO -->";
+            var result = conteudo.Split(separadores, StringSplitOptions.None);
+
+            if (result.Count() != 3)
+                throw new Exception($"Conteúdo fora do padrao. Não foi possível extrair o conteudo.");
+
+            return result[1];
+        }
+
+        public static string ExtrairSuttaArquivo(string arquivo)
+        {
+            string textoTodo = File.ReadAllText(arquivo, Util.encoding);
+
+            return ExtrairSuttaConteudo(textoTodo);
+        }
+
+        public static void RemoverAvisoDistribuicao(ref string html)
+        {
+            var padrao = @"(\<b\>){0,1}( )*Somente para distribuição gratuita\.[a-zA-ZÀ-ÿ \/\-\.\<\>|\r\n]+De outra forma todos os direitos estão reservados\.( )*(\<\/i\>){0,1}";
+            Regex r = new Regex(padrao);
+            Match m = r.Match(html);
+
+            if (m.Success)
+                html = html.Remove(m.Index, m.Length);
+        }
+
+
+        private static string PegarTextoLink(string linha, string path)
+        {
+            string conteudo = "";
+            var padrao = @"href=\""[a-zA-Z0-9 \.\/_]+\""";
+            Regex r = new Regex(padrao);
+            Match m = r.Match(linha);
+
+            if (m.Success){
+                var caminho = path + m.ToString().Replace("href=\"","").Replace("\"","");
+                conteudo = LimparArquivo(caminho, false);
+                RemoverAvisoDistribuicao(ref conteudo);
+                conteudo = Util.ExtrairSuttaConteudo(conteudo);
+            }
+
+            return conteudo;
+        }
+    }
+}
